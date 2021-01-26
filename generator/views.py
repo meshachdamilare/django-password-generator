3edff13ad6f93e33fee70bb640a516c42d9777d1
Christomesh <meshacholuwadamilare@gmail.com>
2021-01-26 11:59:05 +0100

    adding form data

--- generator/views.py
+++ generator/views.py
@@ -11,7 +11,17 @@ def home(request):
 def password(request):
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
-    length = 10
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+'))
+    
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length', 12))
     thepassword = ''
     for x in range(length):
         thepassword += random.choice(characters)
